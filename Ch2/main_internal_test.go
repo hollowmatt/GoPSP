47a8ceb14e496ab740e690d7a77f16c4e12862e4
hollowmatt <hollowmatt@gmail.com>
2023-12-25 15:52:58 +0000

    add new test

--- Ch2/main_internal_test.go
+++ Ch2/main_internal_test.go
@@ -1,7 +1,19 @@
 package main
 
+import "testing"
+
 func ExampleMain() {
 	main()
 	// Output:
 	// Hello world
+}
+
+func TestGreet(t *testing.T) {
+	want := "Hello world"
+	got := greet()
+
+	if got != want {
+		// mark test as failed
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
 }
