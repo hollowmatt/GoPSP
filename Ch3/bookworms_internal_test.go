15aff150a7c5b517d2f52b35cd83fbff6054e587
hollowmatt <hollowmatt@gmail.com>
2023-12-26 21:29:56 +0000

    test cases

--- Ch3/bookworms_internal_test.go
+++ Ch3/bookworms_internal_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -23,10 +22,20 @@ func TestLoadBookworms(t *testing.T) {
 		"file exists": {
 			bookwormsFile: "testdata/bookworms.json",
 			want: []Bookworm{
-				{Name: "Matt", Books: []Book{soul, jetBlackHeart, traffic}},
 				{Name: "Sheila", Books: []Book{jetBlackHeart, paperWhite, traffic}},
+				{Name: "Matt", Books: []Book{soul, jetBlackHeart, traffic}},
 			},
 			wantErr: false,
+		},
+		"file doesn't exist": {
+			bookwormsFile: "testdata/nofile.json",
+			want:          nil,
+			wantErr:       true,
+		},
+		"invalid JSON": {
+			bookwormsFile: "testdata/badfile.json",
+			want:          nil,
+			wantErr:       true,
 		},
 	}
 
@@ -36,7 +45,7 @@ func TestLoadBookworms(t *testing.T) {
 
 			if tc.wantErr {
 				if err == nil {
-					t.Fatal("expected err, bot noghitn")
+					t.Fatalf("expected err, got nothing")
 				}
 				return
 			}
@@ -45,8 +54,47 @@ func TestLoadBookworms(t *testing.T) {
 				t.Fatalf("expected no error, got %v", err)
 			}
 
-			//all good
-			fmt.Println(got)
+			//compare contentss against expected
+			if !equalBookworms(got, tc.want) {
+				t.Fatalf("results do not match, got: %v, expected %v", got, tc.want)
+			}
 		})
 	}
+}
+
+// compare two lists of bookworms, return true if they match
+func equalBookworms(bookworms, target []Bookworm) bool {
+	//compare length
+	if len(bookworms) != len(target) {
+		return false
+	}
+
+	//iterate and compare
+	for i := range bookworms {
+		// chack index name match
+		if bookworms[i].Name != target[i].Name {
+			return false
+		}
+		//check book list for the user
+		if !equalBooks(bookworms[i].Books, target[i].Books) {
+			return false
+		}
+	}
+	return true
+}
+
+func equalBooks(books, target []Book) bool {
+	//compare length
+	if len(books) != len(target) {
+		return false
+	}
+
+	//iterate and compare
+	for i := range books {
+		if (books[i].Author != target[i].Author) && (books[i].Title != target[i].Title) {
+			return false
+		}
+	}
+
+	return true
 }
