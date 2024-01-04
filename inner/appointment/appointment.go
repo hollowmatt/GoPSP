4c7419fee319dffe8a83dd76d181fa5753da7094
hollowmatt <hollowmatt@gmail.com>
2024-01-04 00:14:19 +0000

    alternative appointment approach

--- inner/appointment/appointment.go
+++ inner/appointment/appointment.go
@@ -19,22 +19,35 @@ func NewAppointment(start time.Time, end time.Time) (*Appointment, error) {
 	return &Appointment{Start: start, End: end}, nil
 }
 
-func (a *Appointment) Equals(b *Appointment) bool {
-	return a.Start == b.Start && a.End == b.End
-}
+// func (a *Appointment) Equals(b *Appointment) bool {
+// 	return a.Start == b.Start && a.End == b.End
+// }
+
+// func FindConflicts(appointments []*Appointment) {
+// 	for _, a := range appointments {
+// 		for _, b := range appointments {
+// 			if a.Equals(b) {
+// 				a.HasConflict = true
+// 				b.HasConflict = true
+// 				continue
+// 			}
+// 			if a.Start.Before(b.Start) && b.End.Before(a.End) {
+// 				a.HasConflict = true
+// 				b.HasConflict = true
+// 			}
+// 		}
+// 	}
+// }
 
 func FindConflicts(appointments []*Appointment) {
-	for _, a := range appointments {
-		for _, b := range appointments {
-			if a.Equals(b) {
-				a.HasConflict = true
-				b.HasConflict = true
-				continue
-			}
-			if a.Start.Before(b.Start) && b.End.Before(a.End) {
-				a.HasConflict = true
-				b.HasConflict = true
-			}
+	var latest *Appointment
+	for _, cur := range appointments {
+		if latest != nil && cur.Start.Before(latest.End) {
+			cur.HasConflict = true
+			latest.HasConflict = true
+		}
+		if latest == nil || cur.End.After(latest.End) {
+			latest = cur
 		}
 	}
 }
