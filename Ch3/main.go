d0fc2586c52abaa9902a11f8d75ab931057fa94b
hollowmatt <hollowmatt@gmail.com>
2024-01-08 23:42:17 +0000

    add display books function

--- Ch3/main.go
+++ Ch3/main.go
@@ -10,14 +10,10 @@ func main() {
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "failed to load bookworms: %s\n", err)
 	}
-	fmt.Println(bookworms)
-
-	books := booksCount(bookworms)
-	fmt.Println(books)
+	//
 
 	common := findCommonBooks(bookworms)
-	for _, book := range common {
-		fmt.Println("Author: " + book.Author + ", Title: " + book.Title)
-	}
+	fmt.Println("Common books are:")
+	displayBooks(common)
 
 }
