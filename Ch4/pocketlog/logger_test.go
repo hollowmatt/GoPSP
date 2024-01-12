1ac0dbb1cb3e79d6afe2d0a4537cec27e714d536
hollowmatt <hollowmatt@gmail.com>
2024-01-12 18:05:11 +0000

    add a test

---
+++ Ch4/pocketlog/logger_test.go
@@ -0,0 +1,11 @@
+package pocketlog_test
+
+import (
+	"hollowmatt.com/logger/pocketlog"
+)
+
+func ExampleLogger_Debugf() {
+	debugLogger := pocketlog.New(pocketlog.LevelDebug)
+	debugLogger.Debugf("Hello, %s", "world")
+	// Output: Hello, world.
+}
