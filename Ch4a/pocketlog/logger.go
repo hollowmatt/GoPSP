3066e7157702ef5ac4317db10c35e8665181a816
Matt Holloway <hollowmatt@gmail.com>
2024-04-07 20:28:35 -0400

    stub out the functions for log levels in logger.go

--- Ch4a/pocketlog/logger.go
+++ Ch4a/pocketlog/logger.go
@@ -2,3 +2,23 @@ package pocketlog
 
 type Logger struct {
 }
+
+func (l *Logger) Debugf(format string, args ...any) {
+	//implement me
+}
+
+func (l *Logger) Infof(format string, args ...any) {
+	//impleent me
+}
+
+func (l *Logger) Warnf(format string, args ...any) {
+	//impleent me
+}
+
+func (l *Logger) Errorf(format string, args ...any) {
+	//impleent me
+}
+
+func (l *Logger) Fatalf(format string, args ...any) {
+	//impleent me
+}
