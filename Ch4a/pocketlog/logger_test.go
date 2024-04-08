69c985f56fe8637f6aefc17fae619ea139ca669c
Matt Holloway <hollowmatt@gmail.com>
2024-04-08 18:43:32 -0400

    fix test

--- Ch4a/pocketlog/logger_test.go
+++ Ch4a/pocketlog/logger_test.go
@@ -3,7 +3,7 @@ package pocketlog_test
 import "hollowmatt/new_logger/pocketlog"
 
 func ExampleLogger_Debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug, nil)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug)
 	debugLogger.Debugf("hello, %s", "world")
 	// Output: hello, world
 }
