264742fdeabd4dc247b990194e0783e01dcca679
Matt Holloway <hollowmatt@gmail.com>
2024-04-08 18:25:48 -0400

    update with io.Writer and refactor methods

--- Ch4a/pocketlog/logger.go
+++ Ch4a/pocketlog/logger.go
@@ -1,15 +1,26 @@
 package pocketlog
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
 // Returns a pointer to a new logger, at the requested threshold level
-func New(threshold Level) *Logger {
+// Default output is Stdout
+func New(threshold Level, output io.Writer) *Logger {
+	if output == nil {
+		output = os.Stdout
+	}
+
 	return &Logger{
 		threshold: threshold,
+		output:    output,
 	}
 }
 
@@ -17,33 +28,37 @@ func (l *Logger) Debugf(format string, args ...any) {
 	if l.threshold > LevelDebug {
 		return
 	}
-	_, _ = fmt.Printf(format+"\n", args...)
+	l.logf(format, args...)
 }
 
 func (l *Logger) Infof(format string, args ...any) {
 	if l.threshold > LevelInfo {
 		return
 	}
-	_, _ = fmt.Printf(format+"\n", args...)
+	l.logf(format, args...)
 }
 
 func (l *Logger) Warnf(format string, args ...any) {
 	if l.threshold > LevelWarn {
 		return
 	}
-	_, _ = fmt.Printf(format+"\n", args...)
+	l.logf(format, args...)
 }
 
 func (l *Logger) Errorf(format string, args ...any) {
 	if l.threshold > LevelError {
 		return
 	}
-	_, _ = fmt.Printf(format+"\n", args...)
+	l.logf(format, args...)
 }
 
 func (l *Logger) Fatalf(format string, args ...any) {
 	if l.threshold > LevelFatal {
 		return
 	}
-	_, _ = fmt.Printf(format+"\n", args...)
+	l.logf(format, args...)
+}
+
+func (l *Logger) logf(format string, args ...any) {
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
 }
--- Ch4a/pocketlog/logger_test.go
+++ Ch4a/pocketlog/logger_test.go
@@ -3,7 +3,7 @@ package pocketlog_test
 import "hollowmatt/new_logger/pocketlog"
 
 func ExampleLogger_Debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug, nil)
 	debugLogger.Debugf("hello, %s", "world")
 	// Output: hello, world
 }
