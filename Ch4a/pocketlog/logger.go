aceb43416b6dc59d30d6f70cda4b2e566978f847
Matt Holloway <hollowmatt@gmail.com>
2024-04-07 20:33:54 -0400

    create a new function, to instantiate and return a pointer to a logger

--- Ch4a/pocketlog/logger.go
+++ Ch4a/pocketlog/logger.go
@@ -1,6 +1,13 @@
 package pocketlog
 
 type Logger struct {
+	threshold Level
+}
+
+func New(threshold Level) *Logger {
+	return &Logger{
+		threshold: threshold,
+	}
 }
 
 func (l *Logger) Debugf(format string, args ...any) {
