3c78ff98f07304375752c5d4b685ba1eb5bd1686
Matt Holloway <hollowmatt@gmail.com>
2024-04-14 11:06:41 -0400

    hint library

---
+++ Ch5/gordle/hint.go
@@ -0,0 +1,36 @@
+package gordle
+
+import "strings"
+
+// hint describes the validity of a character in a guess.
+type hint byte
+
+// feedback is the array of hints for the length of the word (guess)
+type feedback []hint
+
+const (
+	absentCharacter hint = iota
+	wrongPostion
+	correctPosition
+)
+
+func (h hint) String() string {
+	switch h {
+	case absentCharacter:
+		return "⬜️"
+	case wrongPostion:
+		return "🟨"
+	case correctPosition:
+		return "🟩"
+	default:
+		return "💩"
+	}
+}
+
+func (f feedback) String() string {
+	result := strings.Builder{}
+	for _, h := range f {
+		result.WriteString(h.String())
+	}
+	return result.String()
+}
