6f3a14273cb884d71e25b7c74b184ac0d5273400
Matt Holloway <hollowmatt@gmail.com>
2024-04-16 10:50:43 -0400

    create error library, add decimal function

--- Ch6/money/decimal.go
+++ Ch6/money/decimal.go
@@ -1,4 +1,15 @@
 package money
 
 type Decimal struct {
+	// subunits is the amount of subunits.  Multiply it by the precision to get the real value
+	subunits int64
+	// number of subunits in a unit, expressed as a power of 10
+	precision byte
+}
+
+ParseDecimal(string) (Decimal, error) {
+	// 1 - find the position of the . and split on it. 
+	// 2 - convert the string without the . to an integer. This could fail
+	// 3 - add some consistency check
+	// 4 - return the result
 }
