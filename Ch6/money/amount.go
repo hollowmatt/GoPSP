ffb8fcc0d3d084b6ee09cab9d48f9c6a8fd8ec7a
Matt Holloway <hollowmatt@gmail.com>
2024-04-18 20:04:50 -0400

    build out the amount library

--- Ch6/money/amount.go
+++ Ch6/money/amount.go
@@ -4,3 +4,15 @@ type Amount struct {
 	quantity Decimal
 	currency Currency
 }
+
+const ErrTooPrecise = Error("quantity is too precice")
+
+// Return an amount of money
+func NewAmount(quantity Decimal, currency Currency) (Amount, error) {
+	if quantity.precision > currency.precision {
+		return Amount{}, ErrTooPrecise
+	}
+
+	quantity.precision = currency.precision
+	return Amount{quantity: quantity, currency: currency}, nil
+}
