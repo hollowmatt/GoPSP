c5dc60f0c77720cd0eadf6f663efd6d4e65f1b8f
Matt Holloway <hollowmatt@gmail.com>
2024-04-21 15:45:08 -0400

    start to setup command line

--- Ch6/main.go
+++ Ch6/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	from := flag.String("from", "", "source currency, required")
+	to := flag.String("to", "GBP", "target currency")
+
+	flag.Parse()
+	fmt.Println(*from, *to)
 }
